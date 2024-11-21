e77e93ec02a614e20841104e0dad1b37d4fe04d3
belka.boo <ibelka.boo@gmail.com>
2024-11-21 17:06:34 +0300

    hw-4-app add solution

--- hw-4-app/src/App.js
+++ hw-4-app/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.png';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
