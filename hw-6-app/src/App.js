9a001858148176e5ed4b63461387828ac81fb554
belka.boo <ibelka.boo@gmail.com>
2024-11-28 15:59:11 +0300

    hw-6-app initial commit

---
+++ hw-6-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h5>Урок 5. Компоненты высшего порядка знакомство с Redux</h5>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
