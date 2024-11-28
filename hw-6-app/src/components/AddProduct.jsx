611df71c1b877c3339a99e4cfedb6f3380eeddac
belka.boo <ibelka.boo@gmail.com>
2024-11-28 18:45:49 +0300

    hw-6-app дал стилька

--- hw-6-app/src/components/AddProduct.jsx
+++ hw-6-app/src/components/AddProduct.jsx
@@ -3,6 +3,13 @@ import { useDispatch } from 'react-redux';
 import { addProduct } from '../store/productSlice';
 import { v4 as uuidv4 } from 'uuid';
 
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Button from '@mui/material/Button';
+
+
 function AddProduct() {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
@@ -12,7 +19,7 @@ function AddProduct() {
     const dispatch = useDispatch();
 
     const handleAddProduct = () => {
-        if (!name || !description || !price) return;
+        if (!name.trim() || !description.trim() || !price || price < 0) return;
 
         dispatch(
             addProduct({
@@ -30,39 +37,56 @@ function AddProduct() {
     };
 
     return (
-        <div>
-            <h2>Добавить продукт</h2>
-            <input
-                type="text"
-                placeholder="Название"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <input
-                type="text"
-                placeholder="Описание"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-            />
-            <input
-                type="number"
-                placeholder="Цена"
-                value={price}
-                onInput={(e) => {
-                    e.target.value = e.target.value.replace(/[^0-9.]/g, '');
-                }}
-                onChange={(e) => setPrice(e.target.value)}
-            />
-            <label>
-                Доступно:
-                <input
-                    type="checkbox"
-                    checked={available}
-                    onChange={(e) => setAvailable(e.target.checked)}
+        <>
+            <Typography variant="h4" component="h2" gutterBottom>
+                Добавить продукт
+            </Typography>
+            <div className='add-product'>
+
+                <TextField
+                    label="Название"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    style={{ margin: '10px' }}
                 />
-            </label>
-            <button onClick={handleAddProduct}>Добавить</button>
-        </div>
+                <TextField
+                    label="Описание"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    style={{ margin: '10px' }}
+                />
+                <TextField
+                    label="Цена"
+                    value={price}
+                    onInput={(e) => {
+                        e.target.value = e.target.value.replace(/[^0-9.]/g, '');
+                    }}
+                    onChange={(e) => setPrice(e.target.value)}
+                    style={{ margin: '10px' }}
+                />
+                <FormControlLabel
+                    style={{ margin: '10px' }}
+                    control={
+                        <Checkbox
+                            checked={available}
+                            onChange={(e) => setAvailable(e.target.checked)}
+                            color="primary"
+
+                        />
+                    }
+                    label="Доступно"
+                />
+
+                <Button
+                    variant="contained"
+                    color="success"
+                    style={{ margin: '10px' }}
+                    onClick={handleAddProduct}
+                >
+                    Добавить
+                </Button>
+            </div>
+        </>
     );
 }
 
--- hw-6-app/src/components/ProductList.jsx
+++ hw-6-app/src/components/ProductList.jsx
@@ -2,27 +2,50 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteProduct, toggleAvailability } from '../store/productSlice';
 
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import DisabledByDefaultIcon from '@mui/icons-material/DisabledByDefault';
+
+
 function ProductList() {
     const products = useSelector((state) => state.products);
     const dispatch = useDispatch();
 
     return (
-        <div>
-            <h2>Список продуктов</h2>
-            {products.map((product) => (
-                <div key={product.id}>
-                    <h3>{product.name}</h3>
-                    <p>{product.description}</p>
-                    <p>Цена: {product.price} <span>$</span></p>
-                    <p>Доступно: {product.available ? 'Да' : 'Нет'}</p>
-                    <button onClick={() => dispatch(toggleAvailability(product.id))}>
-                        Переключить доступность
-                    </button>
-                    <button onClick={() => dispatch(deleteProduct(product.id))}>Удалить</button>
-                </div>
-            ))}
-        </div>
+        <>
+            <h4>Список товаров</h4>
+            <div className='products'>
+                {products.map((product) => (
+                    <div key={product.id} className='product-card'>
+                        <h3>{product.name}</h3>
+                        <p>{product.description}</p>
+                        <p>Цена: {product.price} <span className='dollar'>$</span></p>
+                        <div className='avaliable'>
+                            <p>Доступность:</p>
+                            {product.available ? <CheckCircleIcon color='success' fontSize='large' /> :
+                                <DisabledByDefaultIcon color='error' fontSize='large' />}
+                        </div>
+                        <Button
+                            variant="contained"
+                            style={{ margin: '0 10px' }}
+                            onClick={() => dispatch(toggleAvailability(product.id))}>
+                            Переключить доступность
+                        </Button>
+                        <IconButton
+                            color="error"
+                            onClick={() => dispatch(deleteProduct(product.id))}
+                        >
+                            <DeleteIcon />
+                        </IconButton>
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }
 
 export default ProductList;
+
+
