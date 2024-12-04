25c2cfe4276f2283886178f2c1544310139b9daf
belka.boo <ibelka.boo@gmail.com>
2024-12-05 00:25:11 +0300

    hw-8-app mvp

--- hw-8-app/src/App.js
+++ hw-8-app/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/style.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import MainPage from "./components/MainPage";
+import CatalogPage from "./components/CatalogPage";
+import CartPage from "./components/CartPage";
+import RegistrationPage from "./components/RegistrationPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} exact />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/registration" element={<RegistrationPage />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
